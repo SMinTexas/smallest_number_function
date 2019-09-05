768e6d298030ad7a570363f9945ee8bb1bb15089
Steven Murray <tstevenm@gmail.com>
2019-09-05 08:09:23 -0500

    Initial Commit

---
+++ smallest.py
@@ -0,0 +1,19 @@
+# Write a function that accepts a list of numbers as an argument.
+# It should return the smallest number in the list.
+
+def smallest(list_of_numbers):
+    minimum = 0
+    for number in list_of_numbers:
+        if number < minimum:
+            minimum = number
+
+    return minimum
+
+
+numbers = [7, 21, 4, 33, -456, 8, 99, 1, 12340, 2, 79, 88, 124, 90]
+
+print(" ")
+print(f"Original List: {numbers}")
+print(" ")
+print(f"New List: {smallest(numbers)}")
+print(" ")
